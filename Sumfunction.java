360befd34c26774905d36f580321b3a994ebd8e5
varshithayempally <varshithayempally@gmail.com>
2025-01-30 21:53:23 +0530

    Add files via upload

---
+++ Sumfunction.java
@@ -0,0 +1,12 @@
+public class Sumfunction {
+    public static int addnumbers(int a,int b) {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int num1=34;
+        int num2=21;
+        int sum=addnumbers(num1,num2);
+        System.out.println("the sum of"+num1+"and"+num2+"is:"+sum);
+    }
+    
+}
